d61b6c09f7240f69c440a41944375c06c1c5bb3a
siard-y <siardypma@gmail.com>
2021-08-04 00:23:36 +0200

    pulse2txt4.py fix

--- pulse2txt4.py
+++ pulse2txt4.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python
-from threading import Thread
-from time import sleep
 import RPi.GPIO as GPIO  # import RPi.GPIO module
 import json
 import os, datetime, atexit, time, sys
@@ -28,7 +26,7 @@ filename_data = ''
 meting_start = datetime.datetime.now()
 nieuwe_meting = True
 datum_previous = datetime.datetime.now()
-counterfile = open("counter.txt", "w")
+log_list = []
 
 # url_json = "http://deskthijs.local/Digizon/Website/IndoorSki.aspx"
 url_json = "https://www.digizon.nl/IndoorSki.aspx"
@@ -62,22 +60,6 @@ def pulse_detected(pin, force=False):
         global nieuwe_meting
         global omtrek_rol_m
 
-        # Wanneer geen puls of nieuw uur, dan nieuwe meting
-        if (
-                (datetime.datetime.now() - datum_previous).total_seconds() > 20 or
-                (datetime.datetime.now().hour != meting_start.hour)
-        ):
-            nieuwe_meting = True
-
-        # Eerste puls, meting start
-        if nieuwe_meting:
-            filename_data = '/home/pi/IndoorSki/pulse2txt/' + datum2string(datetime.datetime.now()) + '.txt'
-            print('######### NIEUWE METING ########### \nFile: ' + filename_data)
-            counter = 0
-            datum_previous = datetime.datetime.now()
-            meting_start = datetime.datetime.now()
-            nieuwe_meting = False
-
         # Previous pulse
         diff_in_milli_secs = (datetime.datetime.now() - datum_previous).total_seconds() * 1000
         datum_previous = datetime.datetime.now()
@@ -97,43 +79,21 @@ def pulse_detected(pin, force=False):
         if delta.total_seconds() > 0:
             gem_snelheid = meting_afstand / delta.total_seconds()
 
-        # insert pulse
-        pulse = (
-            datum2string(meting_start),
-            datum2string(datetime.datetime.now()),
-            datum2string(datum_previous),
-            str(counter),
-            str(counter_totaal),
-            str(round(diff_in_milli_secs, 2)),
-            str(round(snelheid, 2)),
-            str(round(meting_afstand, 2)),
-            str(round(meting_afstand_totaal, 2)),
-            str(round(gem_snelheid, 2))
-        )
-
-        # Compose Json
-        pulse_json = {
-            "meting_start": pulse[0],
-            "datum": pulse[1],
-            "datum_previous": pulse[2],
-            "counter": pulse[3],
-            "counter_totaal": pulse[4],
-            "diff_in_milli_secs": pulse[5],
-            "snelheid": pulse[6],
-            "meting_afstand": pulse[7],
-            "meting_afstand_totaal": pulse[8],
-            "gem_snelheid": pulse[9]
-        }
-
-        # Display
         counter += 1
         counter_totaal += 1
-        counterfile.write(f"{datetime.datetime.now()}, {counter_totaal}\n")
-        if counter % 100 == 0:
-            counterfile.flush()
+
+        log_list.append(f"{delta}, {datetime.datetime.now()}, {diff_in_milli_secs:0.2f}, {counter_totaal}\n")
+
+        if len(log_list) == 40:
+            with open("counter.txt", "a") as counterfile:
+                for line in log_list:
+                    counterfile.write(f"{line}")
+            log_list.clear()
+
+
 
 # On input change, run input_Chng function
-GPIO.add_event_detect(7, GPIO.RISING, callback=pulse_detected, bouncetime=50)
+GPIO.add_event_detect(7, GPIO.RISING, callback=pulse_detected, bouncetime=10)
 
 # Ready for meting!
 print('Ready!')
@@ -149,4 +109,3 @@ try:
 
 except KeyboardInterrupt:  # trap a CTRL+C keyboard interrupt
     GPIO.cleanup()  # resets all GPIO ports used by this program
-    counterfile.close()
